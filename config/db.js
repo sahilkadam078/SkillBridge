38d58e599dfa029b4633b045df32b91751bdfe48
sahilkadam078 <sahilkadam078@gmail.com>
2026-03-11 11:01:57 +0530

    fIX RENDER PORT

--- config/db.js
+++ config/db.js
@@ -11,6 +11,7 @@ for (const key of requiredEnv) {
 }
 
 const db = mysql.createPool({
+  port: process.env.DB_PORT,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
